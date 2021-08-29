4773ee400de92d85085e8f12f08d914cbde2a088
Anton-Miheev <82051303+Anton-Miheev@users.noreply.github.com>
2021-08-30 02:27:40 +0300

    Update TriangleSquareCalculationTest.java

--- src/main/java/lesson4/TriangleSquareCalculationTest.java
+++ src/main/java/lesson4/TriangleSquareCalculationTest.java
@@ -2,7 +2,6 @@ package lesson4;
 
 public class TriangleSquareCalculationTest {
     static double countTriangleArea(double sideA, double sideB, double sideC) throws BadTriangleException {
-        //вырожденный
         if (sideA < 0 || sideB < 0 || sideC < 0) throw new BadTriangleException();
         double halfP = (sideA + sideB + sideC) / 2;
         double square = Math.sqrt(halfP * (halfP - sideA) * (halfP - sideB) * (halfP - sideC));
